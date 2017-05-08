8ae37c2d12a071e82c9af63ea0b479357420f91b
Jonatas Figueiredo Alves <Jonatas Figueiredo Alves>
2017-05-08 12:50:51 -0300

    Adicionado arquivos datatable. Datatable implementado na tela de localidade uf

--- public/js/sinbio/localidade-loc-uf.js
+++ public/js/sinbio/localidade-loc-uf.js
@@ -1,113 +1,65 @@
-$().ready(function() {
-	$(".grid").flexigrid({
-		url : '/uf/gera-xml',
-		dataType : 'xml',
-		colModel : [ {
-			display : 'Id',
-			name : 'id',
-			width : 40,
-			sortable : true,
-			align : 'center'
-		}, {
-			display : 'Sigla',
-			name : 'sigla',
-			width : 300,
-			sortable : true,
-			align : 'left'
-		}, {
-			display : 'Nome',
-			name : 'nm_uf',
-			width : 300,
-			sortable : true,
-			align : 'left'
-		}],
-		buttons : [ {
-			name : 'Novo',
-			bclass : 'add',
-			onpress : novoModulo
-		}, {
-			name : 'Alterar',
-			bclass : 'edit',
-			onpress : alterarModulo
-		}, {
-			name : 'Excluir',
-			bclass : 'delete',
-			onpress : excluirModulo
-		},
-		 {
-			separator : true
-		} ],
-		searchitems : [ {
-			display : 'Nome',
-			name : 'nm_uf',
-			isdefault : false
-		}
-		],
-		sortname : "nm_uf",
-		sortorder : "asc",
-		usepager : true,
-		useRp : true,
-		rp : 15,
-		showTableToggleBtn : true,
-		width : '100%',
-		height : 460
-	});
-});
+    function novoModulo() {
+        $.post('/uf/verifica-permissao', {sOP: "Cadastrar"}, function (data) {
+            if (data) {
+                $(window.document.location).attr('href', "/uf/cadastrar");
+            }
+            else {
+                jAlert("Voce não possui permissão de acessar essa área.", "Acesso Negado");
+            }
+        });
+    }
 
+    function alterarModulo() {
+        $.post('/uf/verifica-permissao', {sOP: "Alterar"}, function (data) {
+            if (data) {
+                if (table.$('tr.selected').length > 1) {
+                    jAlert("Você deve selecionar apenas um item para alterar.", "Atenção");
+                }
+                else if (table.$('tr.selected').length == 0) {
+                    jAlert("Você deve selecionar um item para alterar.", "Atenção");
+                }
+                else {
+                    var nId = table.row(table.$('tr.selected')).data()[0];
+                    $(window.document.location).attr('href', "/uf/alterar/nId/" + nId);
+                }
+            }
+            else {
+                jAlert("Voce não possui permissão de acessar essa área.", "Acesso Negado");
+            }
+        });
+    }
 
-function novoModulo() {
-	$.post('/uf/verifica-permissao', {sOP: "Cadastrar"} ,function(data) {
-		if (data) {
-			$(window.document.location).attr('href',"/uf/cadastrar");
-		}
-		else {
-			jAlert("Voce não possui permissão de acessar essa área.","Acesso Negado");
-		}
-	});
-}
+    function excluirModulo() {
+        if ($('tr.selected').length == 0) {
+            jAlert("Você deve selecionar ao menos um ítem.", "Atenção");
+        }
+        else {
+            var info = [];
+            var columnInfo;
+            table.$('tr.selected').each(function () {
+                columnInfo = table.row(this).data()[2];
+                info.push(columnInfo);
+            });
 
-function alterarModulo() {
-	$.post('/uf/verifica-permissao', { sOP: "Alterar" }, function(data) {
-		if (data) {
-			if ($('.trSelected').length > 1) {
-				jAlert("Você deve selecionar apenas um item para alterar.","Atenção");
-			}
-			else if ($('.trSelected').length == 0) {
-				jAlert("Você deve selecionar um item para alterar.","Atenção");
-			}
-			else {
-				var nId = $('.trSelected td:first-child div').text();
-				$(window.document.location).attr('href',"/uf/alterar/nId/"+nId);
-			}
-		}
-		else {
-			jAlert("Voce não possui permissão de acessar essa área.","Acesso Negado");
-		}
-	});
-}
+            if (confirm('Você tem certeza que deseja excluir o(s) estado(s): ' + info + ' ?')) {
+                $.post('/uf/verifica-permissao', {sOP: "Excluir"}, function (data) {
+                    if (data) {
+                        var selArry = [];
+                        var nId;
 
-function excluirModulo() {
-	if ($('.trSelected').length == 0) {
-		jAlert("Você deve selecionar ao menos um ítem.","Atenção");
-	}
-	else {
-		if (confirm('Você tem certeza que deseja excluir ' + $('.trSelected').length + ' UFs(s)?')) {
-			$.post('/uf/verifica-permissao', { sOP: "Excluir" } ,function(data) {
-				if (data) {
-					var selArry = [];
-					var nI = 1;
-					$("div.flexigrid .trSelected").each(function(trI,tr){
-						selArry.push($(tr).attr("id").substr(3));
-					});
-					$.post('/uf/excluir', { fId: selArry, sOP: "Excluir" } ,function(data) {
-						location.reload();
-					});
-				}
-				else {
-					jAlert("Voce não possui permissão de acessar essa área.","Acesso Negado");
-				}
-			});
-		}
-	}
-}
+                        table.$('tr.selected').each(function () {
+                            nId = table.row(this).data()[0];
+                            selArry.push(nId);
+                        });
 
+                        $.post('/uf/excluir', {fId: selArry, sOP: "Excluir"}, function (data) {
+                            location.reload();
+                        });
+                    }
+                    else {
+                        jAlert("Voce não possui permissão de acessar essa área.", "Acesso Negado");
+                    }
+                });
+            }
+        }
+    }
---
+++ public/js/sinbio/tabelas-datatable.js
@@ -0,0 +1,136 @@
+$().ready(function () {
+
+    table = $('.tabela').DataTable({
+        responsive: true,
+        dom: 'rB<"container-pes-bas"><"container-pes-avan">tip',
+        buttons: [
+            {
+                text: 'Novo',
+                action: function () {
+                    novoModulo();
+                }
+            },
+            {
+                text: 'Editar',
+                action: function () {
+                    alterarModulo();
+                }
+            },
+            {
+                text: 'Apagar',
+                action: function () {
+                    excluirModulo();
+                }
+            }
+        ],
+        columnDefs: [
+            {
+                "targets": [0],
+                "visible": false,
+                "searchable": false
+            }
+        ],
+        initComplete: function () {
+            $('.tabela tbody').on('click', 'tr', function () {
+                if ($(this).hasClass('selected')) {
+                    $(this).removeClass('selected');
+                }
+                else {
+                    $(this).addClass('selected');
+                }
+            });
+        }
+
+    });
+
+    criarPesqBasica();
+    criarPesqAvan();
+
+});
+
+//Métodos para troca de filtros ao clique dos botões
+function pesquisaAvancada() {
+    clearInput();
+
+    $('.container-pes-avan').show();
+    $('.container-pes-bas').hide();
+}
+
+function pesquisaBasica() {
+    clearInput();
+
+    $('.container-pes-bas').show();
+    $('.container-pes-avan').hide();
+
+}
+
+//Cria filtro e botão utilizado no container de pesquisa básica
+function criarPesqBasica() {
+
+    var htmlFiltro = '<input class="filtro-basico" type="text" placeholder="Buscar">';
+    $('.container-pes-bas').append(htmlFiltro);
+
+    $('.filtro-basico').keyup(function () {
+        table.search($(this).val()).draw();
+    });
+
+    new $.fn.dataTable.Buttons(table, {
+        buttons: [
+            {
+                text: 'Pesquisa Avançada',
+                action: function () {
+                    pesquisaAvancada();
+                }
+            }
+        ]
+    });
+
+    table.buttons(1, null).container().appendTo('.container-pes-bas');
+
+    $('.container-pes-bas').addClass('pull-right');
+}
+
+//Cria botão para retorno à pesquisa básica, filtros para cada coluna e adiona-os ao container
+function criarPesqAvan() {
+
+    $('.tabela th').each(function () {
+
+        var column = $(this);
+        var htmlFiltro = '<input id="filtro-comp' + column.index() + '" class="filtro-composto" type="text" placeholder="Procurar em ' + column.text() + '"/>';
+
+        $('.container-pes-avan').append(htmlFiltro);
+        $('#filtro-comp' + column.index() + '').keyup(function () {
+            table.column(column.index() + 1).search($(this).val()).draw();
+        });
+
+    });
+
+    new $.fn.dataTable.Buttons(table, {
+        buttons: [
+            {
+                text: 'Pesquisa Básica',
+                action: function () {
+                    pesquisaBasica();
+                }
+            }
+        ]
+    });
+
+    table.buttons(2, null).container().appendTo('.container-pes-avan');
+
+    $('.container-pes-avan').addClass('pull-right');
+    $('.container-pes-avan').hide();
+
+
+    }
+    
+    function clearInput() {
+
+        $('.filtro-basico').val('');
+        table.search('').draw();
+        $('.tabela th').each(function () {
+            var column = $(this).index();
+            $('#filtro-comp' + column + '').val('');
+            table.columns().search('').draw();
+        });
+}
